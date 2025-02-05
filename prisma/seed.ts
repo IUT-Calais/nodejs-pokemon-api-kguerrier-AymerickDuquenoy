36f5fd839670283f5c12eb5014138b19f37fdbcb
Aymerick Duquenoy <aymduqu@gmail.com>
2025-02-05 17:20:20 +0100

    Modification Type

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -8,43 +8,42 @@ async function main() {
   await prisma.type.deleteMany();
   await prisma.type.createMany({
     data: [
-      { name: 'Normal' },
-      { name: 'Fire' },
-      { name: 'Water' },
-      { name: 'Grass' },
-      { name: 'Electric' },
-      { name: 'Ice' },
-      { name: 'Fighting' },
-      { name: 'Poison' },
-      { name: 'Ground' },
-      { name: 'Flying' },
-      { name: 'Psychic' },
-      { name: 'Bug' },
-      { name: 'Rock' },
-      { name: 'Ghost' },
-      { name: 'Dragon' },
-      { name: 'Dark' },
-      { name: 'Steel' },
-      { name: 'Fairy' },
+      {id:1, name: 'Normal' },
+      {id:2,  name: 'Fire' },
+      {id:3,  name: 'Water' },
+      { id:4, name: 'Grass' },
+      {id:5,  name: 'Electric' },
+      {id:6,  name: 'Ice' },
+      {id:7,  name: 'Fighting' },
+      {id:8,  name: 'Poison' },
+      { id:9, name: 'Ground' },
+      {id:10,  name: 'Flying' },
+      { id:11, name: 'Psychic' },
+      { id:12, name: 'Bug' },
+      { id:13, name: 'Rock' },
+      {id:14,  name: 'Ghost' },
+      {id:15, name: 'Dragon' },
+      {id:16,  name: 'Dark' },
+      {id:17,  name: 'Steel' },
+      {id:18,  name: 'Fairy' },
     ],
   });
 
-  const typeRecords = await prisma.type.findMany();
-  const typeMap = Object.fromEntries(typeRecords.map((t) => [t.name, t.id]));
+
  
   await prisma.pokemonCard.createMany({
     data:[
-      {name: 'Bulbizarre',pokedexId : 1, lifePoints : 39,typeID : typeMap['Grass'] ,size : 70,weight : 6.9},
-      {name: 'Salameche',pokedexId : 4, lifePoints : 39,typeID : typeMap['Fire'], size :60, weight : 8.5},
-      {name: 'Carapuce',pokedexId : 7, lifePoints : 39,typeID : typeMap['Water'], size : 50, weight : 9.0 },
+      {name: 'Bulbizarre',pokedexId : 1, lifePoints : 39,typeID : 4 ,size : 70,weight : 6.9},
+      {name: 'Salameche',pokedexId : 4, lifePoints : 39,typeID : 2, size :60, weight : 8.5},
+      {name: 'Carapuce',pokedexId : 7, lifePoints : 39,typeID : 3, size : 50, weight : 9.0 },
 
-      {name: 'Herbizarre',pokedexId : 2, lifePoints : 60,typeID : typeMap['Grass'] , size : 100, weight : 13 },
-      {name: 'Reptincel',pokedexId : 5, lifePoints : 60,typeID : typeMap['Fire'], size : 110, weight : 19 },
-      {name: 'Carabaffe',pokedexId : 8, lifePoints : 60,typeID : typeMap['Water'], size : 100, weight : 22.5 },
+      {name: 'Herbizarre',pokedexId : 2, lifePoints : 60,typeID : 4 , size : 100, weight : 13 },
+      {name: 'Reptincel',pokedexId : 5, lifePoints : 60,typeID : 2, size : 110, weight : 19 },
+      {name: 'Carabaffe',pokedexId : 8, lifePoints : 60,typeID :3 , size : 100, weight : 22.5 },
 
-      {name: 'Florizarre',pokedexId : 3, lifePoints : 80,typeID : typeMap['Grass'], size :200 , weight : 100  },
-      {name: 'Dracaufeu',pokedexId : 6, lifePoints : 80,typeID : typeMap['Fire'], size : 170, weight : 90.5 },
-      {name: 'Tortank',pokedexId : 9, lifePoints : 80,typeID : typeMap['Water'], size : 160, weight : 85.5 },
+      {name: 'Florizarre',pokedexId : 3, lifePoints : 80,typeID : 4, size :200 , weight : 100  },
+      {name: 'Dracaufeu',pokedexId : 6, lifePoints : 80,typeID : 2, size : 170, weight : 90.5 },
+      {name: 'Tortank',pokedexId : 9, lifePoints : 80,typeID : 3, size : 160, weight : 85.5 },
     ]
   });
 
