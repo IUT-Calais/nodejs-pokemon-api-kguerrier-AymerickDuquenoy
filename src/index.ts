db4ee45a0b516258ff052cf540d5b313b40c95c1
Aymerick Duquenoy <aymduqu@gmail.com>
2025-02-05 09:07:55 +0100

    Routes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,26 @@
 import express from 'express';
+import { Request, Response } from 'express';
 
 export const app = express();
 const port = process.env.PORT || 3000;
 
+
 app.use(express.json());
 
 export const server = app.listen(port);
+
+app.get('/pokemons-cards', (_req: Request, res: Response) => {
+  res.status(200).send('Liste des pokémons');
+  });
+
+app.get('/pokemons-cards/:pokemonCardID', (_req: Request, res: Response) => {
+  res.status(200).send('Votre pokémon porte le numéro :');
+  });
+
+app.post('/pokemons-cards', (_req: Request, res: Response) => {
+  res.send(`Le pokémon enregistrer est  ${_req.body.name}`);
+  });
+
 
 export function stopServer() {
   server.close();
