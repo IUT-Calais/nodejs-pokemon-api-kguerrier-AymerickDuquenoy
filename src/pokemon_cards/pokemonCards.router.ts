f51818f0a5861d833b126c87648b6fb47ff742db
Aymerick Duquenoy <aymduqu@gmail.com>
2025-03-05 15:09:58 +0100

    Ajout_du_middleware

--- src/pokemon_cards/pokemonCards.router.ts
+++ src/pokemon_cards/pokemonCards.router.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { getPokemon, getPokemonId, postPokemon,patchPokemon, deletePokemon } from './pokemonCards.controller'; // Ensure correct path
+import { verifyJWT } from '../common/jwt.middleware';
 
 export const pokemonRouter = Router();
 
 pokemonRouter.get('/', getPokemon);
 pokemonRouter.get('/:id', getPokemonId);
-pokemonRouter.post('/', postPokemon);
-pokemonRouter.patch('/:id', patchPokemon);
-pokemonRouter.delete('/:id', deletePokemon);
+pokemonRouter.post('/',verifyJWT, postPokemon);
+pokemonRouter.patch('/:id',verifyJWT, patchPokemon);
+pokemonRouter.delete('/:id',verifyJWT, deletePokemon);
